dc746c7b38d6dc14782f5ecddc8f3ab5441a619a
klesniak1 <kacper.lesniak@tratif.com>
2025-04-20 18:18:29 +0200

    Fixed corrupted background on login form. Fixed CORS problems. TODO: Create missing pages and add routing.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -16,7 +16,7 @@ import {useAuth} from "../utils/AuthContext";
 import {useState} from "react";
 
 export function LoginForm() {
-    const { login } = useAuth();
+    const {login} = useAuth();
 
     const loginForm = useForm({
         mode: 'uncontrolled',
@@ -33,7 +33,7 @@ export function LoginForm() {
     const [error, setError] = useState<string | null>(null); // To handle login errors
 
     const handleSubmit = async (values: { username: string; password: string }) => {
-        const { username, password } = values;
+        const {username, password} = values;
         try {
             await login(username, password); // Call login function
             setError(null); // Clear any previous error
@@ -43,40 +43,38 @@ export function LoginForm() {
     };
 
     return (
-        <RootLayout>
-            <Paper bg={alpha("white", 0.20)} shadow="lg" p="xl" radius="md">
-                <Center>
-                    <Title size="25" c="white" mb="md">Login page</Title>
-                </Center>
-                <form onSubmit={loginForm.onSubmit(handleSubmit)}>
-                    <Flex gap="sm" w="18vw" pb="xs" direction="column">
-                        <Box>
-                            <InputLabel c="white" size="md">E-mail</InputLabel>
-                            <TextInput
-                                withAsterisk
-                                size="md"
-                                placeholder="username"
-                                key={loginForm.key('username')}
-                                {...loginForm.getInputProps('username')}
-                            />
-                        </Box>
-                        <Box>
-                            <InputLabel c="white" size="md">Password</InputLabel>
-                            <TextInput
-                                size="md"
-                                withAsterisk
-                                placeholder="password"
-                                key={loginForm.key('password')}
-                                {...loginForm.getInputProps('password')}
-                            />
-                        </Box>
+        <Paper bg={alpha("white", 0.20)} shadow="lg" p="xl" radius="md">
+            <Center>
+                <Title size="25" c="white" mb="md">Login page</Title>
+            </Center>
+            <form onSubmit={loginForm.onSubmit(handleSubmit)}>
+                <Flex gap="sm" w="18vw" pb="xs" direction="column">
+                    <Box>
+                        <InputLabel c="white" size="md">E-mail</InputLabel>
+                        <TextInput
+                            withAsterisk
+                            size="md"
+                            placeholder="username"
+                            key={loginForm.key('username')}
+                            {...loginForm.getInputProps('username')}
+                        />
+                    </Box>
+                    <Box>
+                        <InputLabel c="white" size="md">Password</InputLabel>
+                        <TextInput
+                            size="md"
+                            withAsterisk
+                            placeholder="password"
+                            key={loginForm.key('password')}
+                            {...loginForm.getInputProps('password')}
+                        />
+                    </Box>
 
-                        <Group justify="flex-end" mt="md">
-                            <Button size="md" type="submit" color="gray">Submit</Button>
-                        </Group>
-                    </Flex>
-                </form>
-            </Paper>
-        </RootLayout>
+                    <Group justify="flex-end" mt="md">
+                        <Button size="md" type="submit" color="gray">Submit</Button>
+                    </Group>
+                </Flex>
+            </form>
+        </Paper>
     )
 }
